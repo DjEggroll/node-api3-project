8adaccf40fc195e99255d6dba5895e999064f18d
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-07-13 18:37:35 -0400

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,32 @@
+// Need to access database for validation functions from user model
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleDateString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp} ${method} to ${url}]`);
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await User.getById(req.params.id)
+    if(!user) {
+      res.status(404).json({
+        message: 'user does not exist'
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'problem finding user'
+    });
+  }
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +38,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
